a8865148687e5990cb18247634f5d350c5b4bac2
nafbeckh <nafbeckhshn@gmail.com>
2023-06-10 09:02:06 +0700

    Update table users

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'nama',
+        'username',
         'password',
+        'foto',
     ];
 
     /**
