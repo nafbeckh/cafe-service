8c83de6e7ae061db8c2fe8eaf655a010da122b70
nafbeckh <nafbeckhshn@gmail.com>
2023-06-10 11:37:39 +0700

    Database Migration

---
+++ database/migrations/2023_06_10_042711_create_menu_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kategori_id');
+            $table->string('nama_menu');
+            $table->integer('harga_jual');
+            $table->foreign('kategori_id')->references('id')->on('kategori_menu')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('menu');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
---
+++ database/migrations/2023_06_10_042915_create_setting_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_cafe');
+            $table->string('alamat');
+            $table->string('path_logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting');
+    }
+}
