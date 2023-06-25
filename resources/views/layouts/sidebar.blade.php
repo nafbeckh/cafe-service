6f3a1a01a918b83c7103bb51088c75bbe070462a
nafbeckh <nafbeckhshn@gmail.com>
2023-06-25 08:36:33 +0700

    Add menu Kategori

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,49 +1,57 @@
 <nav class="mt-2">
-  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+	<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     <li class="nav-item">
         <a href="{{ route('dashboard') }}" class="nav-link {{ $title == 'Dashboard' ? 'active' : '' }}">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard</p>
         </a>
     </li>
+
     <li class="nav-header">DATA MASTER</li>
+    <li class="nav-item">      
+        <a href="{{ route('kategori.index') }}" class="nav-link {{ $title == 'Kategori Menu' ? 'active' : '' }}">
+          <i class="nav-icon fas fa-list-ol"></i>
+          <p>Kategori Menu</p>
+      </a>
+    </li>
+
     <li class="nav-item">
-      <a href="{{ route('meja.index') }}" class="nav-link {{ $title == 'Data Meja' ? 'active' : '' }}">
-          <i class="nav-icon fa fa-store-alt"></i>
-          <p>Meja</p>
+        <a href="" class="nav-link">
+            <i class="nav-icon fas fa-hamburger"></i>
+            <p>Menu</p>
+        </a>
+      </li>
+
+    <li class="nav-item">
+        <a href="{{ route('meja.index') }}" class="nav-link {{ $title == 'Meja' ? 'active' : '' }}">
+            <i class="nav-icon fa fa-store-alt"></i>
+            <p>Meja</p>
+        </a>
+    </li>
+
+    <li class="nav-header">LAPORAN</li>
+    <li class="nav-item">
+      <a href="" class="nav-link {{ $title == 'Laporan Pemesanan' ? 'active' : '' }}">
+          <i class="nav-icon fas fa-file-alt"></i>
+          <p>Laporan Pemesanan</p>
       </a>
-  </li>
-
-  <li class="nav-item">
-      <a href="" class="nav-link">
-          <i class="nav-icon fas fa-hamburger"></i>
-          <p>Menu</p>
+    </li>
+  
+    <li class="nav-header">SISTEM</li>
+    <li class="nav-item">
+      <a href="" class="nav-link {{ $title == 'Profile' ? 'active' : '' }}">
+          <i class="nav-icon fas fa-users"></i>
+          <p>User</p>
       </a>
-  </li>
-
-  <li class="nav-header">LAPORAN</li>
-  <li class="nav-item">
-    <a href="" class="nav-link {{ $title == 'Laporan Pemesanan' ? 'active' : '' }}">
-        <i class="nav-icon fas fa-file-alt"></i>
-        <p>Laporan Pemesanan</p>
-    </a>
-  </li>
-
-  <li class="nav-header">SISTEM</li>
-  <li class="nav-item">
-    <a href="" class="nav-link {{ $title == 'Profile' ? 'active' : '' }}">
-        <i class="nav-icon fas fa-users"></i>
-        <p>User</p>
-    </a>
-  </li>
-
-  @hasrole('admin')
-  <li class="nav-item">
-      <a href="{{ route('setting.cafe') }}" class="nav-link {{ $title == 'Setting Cafe' ? 'active' : '' }}">
-          <i class="nav-icon fas fa-store"></i>
-          <p>Cafe</p>
-      </a>
-  </li>
-  @endhasrole
-</ul>
+    </li>
+  
+    @hasrole('admin')
+    <li class="nav-item">
+        <a href="{{ route('setting.cafe') }}" class="nav-link {{ $title == 'Setting Cafe' ? 'active' : '' }}">
+            <i class="nav-icon fas fa-store"></i>
+            <p>Cafe</p>
+        </a>
+    </li>
+    @endhasrole
+  </ul>
 </nav>
