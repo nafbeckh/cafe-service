5213486efc3efe254dac77d61d60fc067f3da6e5
nafbeckh <nafbeckhshn@gmail.com>
2023-07-18 21:14:14 +0700

    Update migration pelanggan

--- database/migrations/2023_07_18_170845_pelanggan.php
+++ database/migrations/2023_07_18_170845_pelanggan.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Penlanggan extends Migration
+class Pelanggan extends Migration
 {
     /**
      * Run the migrations.
